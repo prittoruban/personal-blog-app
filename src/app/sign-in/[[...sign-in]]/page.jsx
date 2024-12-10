dbf191c8ffa27b361d174befd77c5f80b2cf570a
R Pritto Ruban <prittoruban0609@gmail.com>
2024-12-10 19:27:29 +0530

    Refresh the repo and start from the scratch

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs';
-
-export default function SignInPage() {
-  return (
-    <div className='flex items-center justify-center p-3'>
-      <SignIn />
-    </div>
-  );
-}
--- src/app/sign-up/[[...sign-up]]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from '@clerk/nextjs';
-
-export default function SignUpPage() {
-  return (
-    <div className='flex items-center justify-center p-3'>
-      <SignUp />
-    </div>
-  );
-}
